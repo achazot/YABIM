a7c1aa243e4c8627ea74443444cdb189a97e1b80
Alban Chazot <alban.chazot@insa-cvl.fr>
2016-02-24 16:06:41 +0100

    debut interface

--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_lin_prompt_returnPressed();
+
 private:
     Ui::MainWindow *ui;
+
+    void parseCommand(QString command);
 };
 
 #endif // MAINWINDOW_H
